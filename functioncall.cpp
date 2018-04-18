0c3eacde6b81ec7f328152edc42c0e8ede2551eb
aswinkumar <37615551+aswinkumarrk@users.noreply.github.com>
2018-04-18 22:21:08 +0530

    Add files via upload

---
+++ functioncall.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void printSomething(){ // Define the function.
+    cout << "I am aswin kumar" << endl;
+}
+int main()
+{
+    printSomething(); // Call the function.
+
+    return 0;
+}
