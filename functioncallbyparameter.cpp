47e3a7a4e7ee908ea73be07d311010844cf441ad
aswinkumar <37615551+aswinkumarrk@users.noreply.github.com>
2018-04-18 22:24:06 +0530

    Add files via upload

---
+++ functioncallbyparameter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// This function takes a parameter.
+void printCrap(int x){
+    cout << " my favorite numbner is " << x << endl;
+}
+
+int main()
+{
+    printCrap(1000000000); // Call the function.
+
+    return 0;
+}
